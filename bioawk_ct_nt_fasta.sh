e5d291fe429578f251bbfa56d249a1f95269a050
Akshay <67769176+Bjorn99@users.noreply.github.com>
2024-08-05 17:03:58 +0530

    add eg_files directory and added script file for counting individual nucleotides using awk and bioawk.

---
+++ bioawk_ct_nt_fasta.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
+# Assign the input filename
+filename=$1
+
+# use awk to count nucleotides A, C, G, T (case-sensitive)
+bioawk -c fastx '{
+    a = g = c = t = 0;
+    for (i = 1; i <= length($0); i++) {
+        nucleotide = substr($0, i, 1);
+        if (nucleotide == "A" || nucleotide == "a") a++;
+        else if (nucleotide == "C" || nucleotide == "c") c++;
+        else if (nucleotide == "G" || nucleotide == "g") g++;
+        else if (nucleotide == "T" || nucleotide == "t") t++;
+    }
+    print "A:", a, "C:", c, "G:", g, "T:", t;
+}' "$filename"
