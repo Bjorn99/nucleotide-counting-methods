0365976e592bce83644877db0cad693f60583cfc
Akshay <67769176+Bjorn99@users.noreply.github.com>
2024-08-05 17:46:07 +0530

    correction in bio-awk script and add a fasta file in eg_files folder

--- awk_ct_nt_fasta.sh
+++ awk_ct_nt_fasta.sh
@@ -10,7 +10,7 @@ filename=$1
 # use awk to count nucleotides A, C, G, T (case-sensitive)
 awk '/^>/ {if (seqlen) print "A:", a, "C:", c, "G:", g, "T:", t;
 a=c=g=t=seqlen=0; next}
-    {for (i = 1; i <= length; i++) {
+    {for (i = 1; i <= length($0); i++) {
         nucleotide = substr($0, i, 1);
         if (nucleotide == "A" || nucleotide == "a") a++;
         else if (nucleotide == "C" || nucleotide == "c") c++;
--- bioawk_ct_nt_fasta.sh
+++ bioawk_ct_nt_fasta.sh
@@ -10,12 +10,14 @@ filename=$1
 # use awk to count nucleotides A, C, G, T (case-sensitive)
 bioawk -c fastx '{
     a = g = c = t = 0;
-    for (i = 1; i <= length($0); i++) {
-        nucleotide = substr($0, i, 1);
+    for (i = 1; i <= length($seq); i++) {
+        nucleotide = substr($seq, i, 1);
+        # Count each nucleotide
         if (nucleotide == "A" || nucleotide == "a") a++;
         else if (nucleotide == "C" || nucleotide == "c") c++;
         else if (nucleotide == "G" || nucleotide == "g") g++;
         else if (nucleotide == "T" || nucleotide == "t") t++;
     }
+    # Print the count of the current sequences
     print "A:", a, "C:", c, "G:", g, "T:", t;
 }' "$filename"
